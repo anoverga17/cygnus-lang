ea6a5055f40ae6ad72548ca3952765ab2d516bf7
anoverga <gabrielanover17@gmail.com>
2024-11-05 21:17:02 -0500

    added lambda to grammar

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "llvm/Support/CommandLine.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Bitcode/BitcodeWriter.h"
+#include "llvm/Support/Host.h"
+
+int main(int args, char **argv) {
+
+    return 0;
+}
